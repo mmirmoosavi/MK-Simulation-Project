91fa8d70a45e799ed15b91d0cfb17ac665bce919
mohammadmirmoosavi <mohammadmirmoosavi@gmail.com>
2023-09-02 16:22:30 +0330

    feat(settings) add urls and also settings for swagger initial configuration and also define static_root for collect static to work with swagger

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="My API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="mohammadmirmoosavi@gmail.com"),
+        license=openapi.License(name="Example License"),
+    ),
+    public=True,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-    path('api/', include('courses.urls'))
+    path('api/', include('courses.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
 ]
